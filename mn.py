2419f9586e5363225aabad34205905002e3390a1
Gourav Dasgupta <97543783+govuhere@users.noreply.github.com>
2023-08-21 15:33:09 +0530

    initial commit

---
+++ mn.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from tkinter import ttk
+import requests
+from playsound import playsound 
+
+
+#city_name="Kolkata"
+#data=requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=e5f1d558732140e0a1c526cc89d90234").json()
+#print(data)
+def data_get():
+    city=city_name.get()
+    data=requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=e5f1d558732140e0a1c526cc89d90234").json()
+    w_label1.config(text=data["weather"][0]["main"])
+    t_label1.config(text=str(round(data["main"]["temp"]-273.15,1)))
+    win.config(bg="lightblue")
+    t_label.config(bg="lightblue")
+    w_label.config(bg="lightblue")
+    name_label.config(bg="lightblue")
+def cleardata():
+    w_label1.config(text="")
+    t_label1.config(text="")
+    win.config(bg="skyblue")
+    t_label.config(bg="skyblue")
+    w_label.config(bg="skyblue")
+    name_label.config(bg="skyblue")
+    
+    
+        
+    
+    
+
+
+win=Tk()
+win.title("GDG APP")
+win.iconbitmap(r'C:\Users\HP\OneDrive\Desktop\pdjg\images.ico')
+win.config(bg="skyblue")
+win.geometry('500x600')
+
+
+name_label=Label(win,text="GDG Weather App",background="skyblue",foreground="black",
+                font=("Arial",30,"bold"))
+name_label.place(x=25,y=50,height=40,width=450) 
+
+city_name=StringVar();
+list_name = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"]
+com=ttk.Combobox(win,text="GDG Weather App",values=list_name,
+                font=("Arial",20,"bold",), textvariable=city_name)
+com.place(x=25,y=130,height=50,width=450) 
+
+
+
+
+
+w_label=Label(win,text="Weather Climate",background="skyblue",foreground="black",
+                font=("Arial",15))
+w_label.place(x=25,y=250,height=30,width=200)
+w_label1=Label(win,text="",foreground="black",
+                font=("Arial",15))
+w_label1.place(x=250,y=250,height=30,width=210)
+
+  
+
+
+t_label=Label(win,text="Temperature ",background="skyblue",foreground="black",
+                font=("Arial",15))
+t_label.place(x=25,y=300,height=30,width=200)
+t_label1=Label(win,text="",foreground="black",
+                font=("Arial",15))
+t_label1.place(x=250,y=300,height=30,width=210)
+
+
+done_button=Button(win,text="Search",background="lightgrey",
+                font=("Aurella",20,"bold",),command=data_get)
+done_button.place(x=40,y=200,height=40,width=200) 
+clr_button=Button(win,text="Clear",background="lightgrey",
+                font=("Aurella",20,"bold",),command=cleardata)
+clr_button.place(x=260,y=200,height=40,width=200) 
+
+##name_label.pack()
+win.mainloop()
