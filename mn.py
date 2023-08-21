0623ab58f28e805db07b98d11192ce41a946a23c
Gourav Dasgupta <97543783+govuhere@users.noreply.github.com>
2023-08-21 15:49:55 +0530

    readme added

--- mn.py
+++ mn.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 import requests
-from playsound import playsound 
 
 
 #city_name="Kolkata"
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Welcome to GDG Weather App
+
+## Build Process
+
+Clone my code and run the following code
+```
+python mn.py
+```
